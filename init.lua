e1428d8927bbfc56b49685455710ce2165e7180c
Arthur Fernandes <arthur0139@gmail.com>
2025-01-01 18:41:13 -0300

    update

--- init.lua
+++ init.lua
@@ -658,10 +658,15 @@ require('lazy').setup({
       -- for you, so that they are available from within Neovim.
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
-        'stylua', -- Used to format Lua code
-        'mypy',
+
+        -- default libs to install on init
         'pydocstyle',
+        'pyright',
+        'autopep8',
+        'html-lsp',
+        'python-lsp-server',
       })
+
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
       require('mason-lspconfig').setup {
